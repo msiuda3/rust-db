549d0609b779f3ddc4b9bdf32b0744df180b77e1
msiuda3 <30621711+msiuda3@users.noreply.github.com>
2024-10-16 14:01:00 +0200

    Delete target directory

--- target/debug/deps/byteorder-5998b5d7f732c5bf.d
+++
@@ -1,6 +0,0 @@
-/Users/msiuda/Desktop/Projekty/rust-db/target/debug/deps/libbyteorder-5998b5d7f732c5bf.rmeta: /Users/msiuda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs /Users/msiuda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs
-
-/Users/msiuda/Desktop/Projekty/rust-db/target/debug/deps/byteorder-5998b5d7f732c5bf.d: /Users/msiuda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs /Users/msiuda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs
-
-/Users/msiuda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs:
-/Users/msiuda/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs:
--- target/debug/deps/rust_db-304cdfff8c5e936b.d
+++
@@ -1,6 +0,0 @@
-/Users/msiuda/Desktop/Projekty/rust-db/target/debug/deps/librust_db-304cdfff8c5e936b.rmeta: src/main.rs src/storage.rs
-
-/Users/msiuda/Desktop/Projekty/rust-db/target/debug/deps/rust_db-304cdfff8c5e936b.d: src/main.rs src/storage.rs
-
-src/main.rs:
-src/storage.rs:
