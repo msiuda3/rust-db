f5276392b9e479bb5f959671880bc4aefeb819e4
msiuda <siudamarcin3@gmail.com>
2024-10-17 00:47:23 +0200

    init

--- target/debug/rust-db.d
+++ target/debug/rust-db.d
@@ -1,1 +1,1 @@
-/Users/msiuda/Desktop/Projekty/rust-db/target/debug/rust-db: /Users/msiuda/Desktop/Projekty/rust-db/src/main.rs /Users/msiuda/Desktop/Projekty/rust-db/src/storage.rs
+/Users/msiuda/Desktop/Projekty/rust-db/rust-db/target/debug/rust-db: /Users/msiuda/Desktop/Projekty/rust-db/rust-db/src/main.rs /Users/msiuda/Desktop/Projekty/rust-db/rust-db/src/storage.rs
