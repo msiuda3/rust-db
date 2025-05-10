ba7d9cffdfa262ba4819f77058eaecf4af7afee7
msiuda <siudamarcin3@gmail.com>
2025-05-10 23:12:18 +0200

    refactored main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use network::reader::{GetMessage, Operation, PutMessage};
 use std::io::{self, Read, Write};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::future::IntoFuture;
 
 mod storage;
 mod network;
@@ -12,69 +13,25 @@ const OPERATION_GET: u8 = 0x01;
 const OPERATION_PUT: u8 = 0x02;
 const OPERATION_GET_RESPONSE: u8 = 0x81;
 
-fn main() -> io::Result<()> {
-    let listener: <<Result<TcpListener, io::Error> as IntoFuture>::Output as Try>::Output = TcpListener::bind("127.0.0.1:7878").await?;
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").await?;
     println!("Server is listening on port 7878...");
     loop{
         let (mut stream, addr) = listener.accept().await?;
         println!("New connection from {:?}", addr);
 
         tokio::spawn(async move{
-            if let Err(e) = handle_client(&mut stream).await {
+            if let Err(e) = handle_message(&mut stream).await {
                 eprintln!("Error occured in connection from: {:?}", e);
             }
-        })
+        });
     }
     Ok(())
 }
 
-fn handle_client(stream: &mut TcpStream) -> io::Result<()> {
-    let mut buffer: Vec<u8> = vec![0; 256]; 
-    let bytes_read = stream.read(&mut buffer)?;
 
-    if bytes_read < 4 {
-        eprintln!("Received message is too short.");
-        return Ok(());
-    }
-
-    let version: u8 = buffer[0];
-    let operation_type: u8 = buffer[1];
-    let key_length: usize = buffer[2] as usize;
-
-    if version != VERSION {
-        eprintln!("Invalid version or operation type.");
-        return Ok(());
-    }
-
-
-    if operation_type == OPERATION_GET{
-
-        let key: &[u8] = &buffer[3..3 + key_length];
-    let key_str = String::from_utf8_lossy(key);
-
-        println!("Received GET request for key: {}", key_str);
-        let mut value = storage::get(&String::from_utf8_lossy(key));
-        let response = create_response(true, &value.get_or_insert("not_found".to_string()));
-        stream.write_all(&response)?;
-
-    }
-    else if operation_type == OPERATION_PUT{
-        let key: &[u8] = &buffer[4..4 + key_length];
-        let key_str = String::from_utf8_lossy(key);
-
-        let val_length: usize = buffer[3] as usize;
-        let value = &buffer[4+key_length..4+key_length+val_length];
-        let value_str = String::from_utf8_lossy(value);
-        println!("Received PUT request for key: {}, value: {}", key_str, value_str);
-        storage::save(&key_str, &value_str);
-        let response = create_response(true, &value_str);
-        stream.write_all(&response)?;
-    }
-    println!("ENDING CONNECTION");
-    Ok(())
-}
-
-fn handle_message(stream: &mut TcpStream) -> io::Result{
+async fn handle_message(stream: &mut TcpStream) -> io::Result<()>{
     let result: Result<network::reader::Operation, network::reader::MessageError> = network::reader::read(stream);
     match result {
         Ok(network::reader::Operation::Get(get_message)) => {
@@ -87,7 +44,7 @@ fn handle_message(stream: &mut TcpStream) -> io::Result{
                     }
         Err(_) => todo!(), //TODO handle case when no matchng operation was found
     }
-    Ok(());
+    Ok(())
 }
 
 fn handle_get(getMessage: GetMessage, stream: &mut TcpStream){
